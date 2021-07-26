ef64c6202c1ae742ee4028ef14b3c7eabb2bd502
David Shears <49477658+DavidShears@users.noreply.github.com>
2021-07-26 12:19:35 +0100

    First draft
    Strip out characters we aren't interested in.

---
+++ app.js
@@ -0,0 +1,26 @@
+// Readline
+
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('error.php'),
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', (string) => {
+	// First test - remove header records by testing for #)
+	if (string.indexOf('#') !== 0) {
+		// Extract date and time
+		var date = string.substring(0,10);
+		var time = string.substring(11,19);
+		// Extract IP address
+		// Starts in position 31, end at next tab character
+		var IPAdd = string.substring(31,string.indexOf('	',31));
+		// Add 46 characters to length - gets us to error message
+		nextpos = IPAdd.length + 46;
+    	console.log(date + ' ' + time + ' ' +
+		IPAdd + ' ' + string.substring(nextpos));
+	}
+});
