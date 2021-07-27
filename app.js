1c30d6c8308b42d3bce4f264cc662f9247ef1ec0
David Shears <49477658+DavidShears@users.noreply.github.com>
2021-07-27 12:18:05 +0100

    Add HTTP status to IIS output

--- app.js
+++ app.js
@@ -57,8 +57,13 @@ rl.on('line', (string) => {
 			var IPStart = string.indexOf(' HTTP');
 			var IPStart = string.lastIndexOf(' ',IPStart - 1);
 			var IPAdd = string.substring(IPStart + 1,string.indexOf(' ',IPStart + 1));
+			// Get HTTP status, currently achieved by looking for substatus
+			// which seems to always return 0
+			var HTTPend = string.indexOf(' 0 ');
+			var HTTPstat = string.substring(HTTPend - 3, HTTPend)
 			// Build CurrentLine from the various elements we've picked up
-			CurrentLine = (IPAdd + ' ' + urlreq);
+			CurrentLine = (IPAdd + ' ' + urlreq + ' ' + HTTPstat);
+		// else use Joomla logic
 		} else {
 			// Starts in position 31, end at next tab character
 			var IPAdd = string.substring(31,string.indexOf('	',31));
@@ -66,6 +71,8 @@ rl.on('line', (string) => {
 			nextpos = IPAdd.length + 46;
 			CurrentLine = (IPAdd + ' ' + string.substring(nextpos));
 		}
+		// Test if record is already in array - if it is then increment counter and update last date
+		// If it isn't then add to arrays and stamp first date
 		if (UniqueRecs.includes(CurrentLine)) {
 			CountRecs[UniqueRecs.indexOf(CurrentLine)] = CountRecs[UniqueRecs.indexOf(CurrentLine)] + 1;
 			LastDate[UniqueRecs.indexOf(CurrentLine)] = date + ' ' + time;
@@ -81,20 +88,39 @@ rl.on('line', (string) => {
 	// Set up workbook and worksheet
 	var workbook = new Excel.Workbook();
 	var worksheet = workbook.addWorksheet("Error Logging");
-	worksheet.columns = [
-		{ header: "IP Address", key:"IPADD"},
-		{ header: "Record", key:"RECORD"},
-		{ header: "Times Found", key:"COUNT"},
-		{ header: "First Found", key:"FIRST"},
-		{ header: "Last Found", key:"LAST"}
-	];
+	// If in IIS mode then include column for HTTP status code
+	if (logtype == 'IIS') {
+		worksheet.columns = [
+			{ header: "IP Address", key:"IPADD"},
+			{ header: "Record", key:"RECORD"},
+			{ header: "HTTP Status", key:"HTTPSTAT"},
+			{ header: "Times Found", key:"COUNT"},
+			{ header: "First Found", key:"FIRST"},
+			{ header: "Last Found", key:"LAST"}
+		]
+	} else {
+		worksheet.columns = [
+			{ header: "IP Address", key:"IPADD"},
+			{ header: "Record", key:"RECORD"},
+			{ header: "Times Found", key:"COUNT"},
+			{ header: "First Found", key:"FIRST"},
+			{ header: "Last Found", key:"LAST"}
+		];
+	}
 	worksheet.getRow(1).font = { name: "Calibri", size: 11, bold: true};
 	// Loop array of unique records
 	var counter = 0;
 	UniqueRecs.forEach(function(element){
+		// Again additional column for IIS records
+		if (logtype == 'IIS') {
 			worksheet.addRow({IPADD: element.substring(0,element.indexOf(' ')), 
-			RECORD: element.substring(element.indexOf(' ')), COUNT: CountRecs[counter], 
-			FIRST: FirstDate[counter], LAST: LastDate[counter]});
+				RECORD: element.substring(element.indexOf(' '),element.lastIndexOf(' ')), HTTPSTAT: element.substring(element.lastIndexOf(' ')),
+				COUNT: CountRecs[counter], FIRST: FirstDate[counter], LAST: LastDate[counter]});
+		} else {
+			worksheet.addRow({IPADD: element.substring(0,element.indexOf(' ')), 
+				RECORD: element.substring(element.indexOf(' ')), COUNT: CountRecs[counter], 
+				FIRST: FirstDate[counter], LAST: LastDate[counter]});
+		}
 		counter++;
 	})
 	workbook.xlsx.writeFile("output.xlsx");
