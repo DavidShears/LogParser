a60aa8a1a44cca8e90ee91e831ba21006c65dd51
David Shears <49477658+DavidShears@users.noreply.github.com>
2021-09-16 16:17:45 +0100

    Expand ipcheck to include more details
    Run stripdown checkbot as well as checkip.
    Also populate spreadsheet with countrycode, domain, and hostname(s) if returned by AbuseIPDB

--- misc scripts/ipcheck.js
+++ misc scripts/ipcheck.js
@@ -9,6 +9,8 @@ var functions = require('../includes/logparse-process.js');
 var checkip = functions.checkip;
 var getip = functions.getip;
 
+var botagents = require('../includes/bots.js').botagents;
+
 var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 
 if (argv.log) {
@@ -42,13 +44,22 @@ var IntNotes = [];
 rl.on('line', (string) => {
 	// Exclude lines beginning with # as they're comments
 	if (string.indexOf('#') !== 0) {
-		// CheckIP used to provide internal notes
 		var IPAdd = getip(string,argv.log);
-		var checkedip = checkip(IPAdd,' ');
 		if (IPs.includes(IPAdd)) { 
 		} else if (IPAdd != null && IPAdd != '') {
 			IPs.push(IPAdd);
-			IntNotes.push(checkedip);
+			// CheckIP & checkbot used to provide internal notes
+			var checkedip = checkip(IPAdd,' ');
+			var checkedbot = checkbot(string);
+			if (checkedip != '' && checkedbot != '') {
+				IntNotes.push(checkedip + '-' + checkedbot)
+			} else if (checkedbot == '') {
+				IntNotes.push(checkedip);
+			} else if (checkedip == '') {
+				IntNotes.push(checkedbot);
+			} else {
+				IntNotes.push('');
+			}
 		}
 	}
 })
@@ -58,8 +69,11 @@ rl.on('line', (string) => {
 	var worksheet = workbook.addWorksheet("Records");
 	var coldef = [];
 	coldef[0] = { header: "IP Address", key:"IPAdd", width: 15};
-	coldef[1] = { header: "Internal Notes", key:"Int", width: 15};
-	coldef[2] = { header: "External Notes", key:"Ext"};
+	coldef[1] = { header: "Physical Location", key:"PhysLoc", width: 17};
+	coldef[2] = { header: "Internal Notes", key:"Int", width: 34};
+	coldef[3] = { header: "Domain", key:"Domain", width: 34};
+	coldef[4] = { header: "Host(s)", key:"Host", width: 55};
+	coldef[5] = { header: "External Notes", key:"Ext", width: 91};
 	worksheet.columns = coldef;
 	worksheet.getRow(1).font = { name: "Calibri", size: 11, bold: true};
 	// Loop array of unique records
@@ -67,8 +81,11 @@ rl.on('line', (string) => {
 	for (let x of IPs) {
 		const rowdef = [];
 		rowdef[0] = x;
-		rowdef[1] = IntNotes[counter];
-		rowdef[2] = '';
+		rowdef[1] = '';
+		rowdef[2] = IntNotes[counter];
+		rowdef[3] = '';
+		rowdef[4] = '';
+		rowdef[5] = '';
 		// Now we ask AbuseIPDB for information
 		const res = await fetch('https://api.abuseipdb.com/api/v2/check?ipAddress=' + x, {
 		// Line below is where you can put the API key you get from the website
@@ -78,13 +95,37 @@ rl.on('line', (string) => {
 		const data = await res.json();
 		// If abuseconfidentscore > 0 then provide details
 		if (data.data.abuseConfidenceScore != 0) {
-			rowdef[2] = 'AbuseIPDB Score: ' + data.data.abuseConfidenceScore
+			rowdef[5] = 'AbuseIPDB Score: ' + data.data.abuseConfidenceScore
 			+ ' based on: ' + data.data.totalReports + ' reports from ' +
 			data.data.numDistinctUsers + ' users, last reported at: '
 			+ data.data.lastReportedAt;
+		}
+		// If a country was provided lets also stamp that
+		if (data.data.countryCode != '') {
+			rowdef[1] = data.data.countryCode;
+		}
+		if (data.data.domain != '') {
+			rowdef[3] = data.data.domain;
+		}
+		if (data.data.hostnames != '') {
+			rowdef[4] = data.data.hostnames;
 		}
 		worksheet.addRow(rowdef);
 		counter++;
 	}
 	workbook.xlsx.writeFile("ipcheck.xlsx");		
 });
+
+// same stripdown version of checkbot from pruner.js
+
+function checkbot(string) {
+	let i = 0;
+	while (botagents[i]) {
+		var tempstring = string.toLowerCase();
+		if (tempstring.indexOf(botagents[i]) != -1) {
+				return(botagents[i] + " address!");
+			}
+		i++;
+		}
+	return("");
+}
