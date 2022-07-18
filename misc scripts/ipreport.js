021084ed02c4031a61651d1826a6c261950c7994
David Shears <49477658+DavidShears@users.noreply.github.com>
2022-07-18 11:04:58 +0100

    Update ipreport.js
    First step towards maybe including bulk-report API - set URL before hitting fetch and move body params inside the function.

--- misc scripts/ipreport.js
+++ misc scripts/ipreport.js
@@ -5,25 +5,31 @@ var argv = require('yargs/yargs')(process.argv.slice(2)).argv;
 
 // --ip=127.0.0.1 --cats=8,23 --comment="test comments"
 
-const params = new URLSearchParams();
+/* Move within report function */
+/* const params = new URLSearchParams();
 params.append('ip',argv.ip);
 params.append('categories',argv.cats);
-params.append('comment',argv.comment);
-
-console.log(params);
+params.append('comment',argv.comment); */
 
 async function report() {
+    const params = new URLSearchParams();
 
-const res = await fetch('https://api.abuseipdb.com/api/v2/report', {
-    method: 'post',
-    // Line below is where you can put the API key you get from the website
-	headers: {'Key': '',
-    'Accept': 'application/json'},
-    body: params
-})
-const data = await res.json();
+        var url = 'https://api.abuseipdb.com/api/v2/report';
 
-console.log(data);
+        params.append('ip',argv.ip);
+        params.append('categories',argv.cats);
+        params.append('comment',argv.comment);
+
+    const res = await fetch(url, {
+            method: 'post',
+            // Line below is where you can put the API key you get from the website
+	        headers: {'Key': '',
+            'Accept': 'application/json'},
+            body: params
+        })
+    const data = await res.json();
+
+    console.log(data);
 }
 
 report();
